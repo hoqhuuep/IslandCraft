338de778f526f4438bbe24afefe7c59c8e9a2c90
Daniel Simmons <daniel@genesis.net.au>
2013-06-18 21:02:57 +1000

    Removed old code

--- src/com/github/hoqhuuep/craftislands/generator/island/IslandGenerator.java
+++
@@ -1,7 +0,0 @@
-package com.github.hoqhuuep.craftislands.generator.island;
-
-import java.awt.image.BufferedImage;
-
-public interface IslandGenerator {
-    public BufferedImage generate(final long random);
-}
