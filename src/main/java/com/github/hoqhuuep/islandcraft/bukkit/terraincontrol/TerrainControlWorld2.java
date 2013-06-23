84b07f9af8fca0f60937dbd48cba3a6efd794750
Daniel Simmons <daniel@genesis.net.au>
2013-06-23 15:33:35 +1000

    New files from last commit

---
+++ src/main/java/com/github/hoqhuuep/islandcraft/bukkit/terraincontrol/TerrainControlWorld2.java
@@ -0,0 +1,22 @@
+package com.github.hoqhuuep.islandcraft.bukkit.terraincontrol;
+
+import com.github.hoqhuuep.islandcraft.common.api.ICWorld2;
+import com.khorn.terraincontrol.LocalWorld;
+
+public class TerrainControlWorld2 implements ICWorld2 {
+    private LocalWorld world;
+
+    public TerrainControlWorld2(final LocalWorld world) {
+        this.world = world;
+    }
+
+    @Override
+    public long getSeed() {
+        return this.world.getSeed();
+    }
+
+    @Override
+    public int biomeId(String name) {
+        return this.world.getBiomeIdByName(name);
+    }
+}
---
+++ src/main/java/com/github/hoqhuuep/islandcraft/common/api/ICWorld2.java
@@ -0,0 +1,7 @@
+package com.github.hoqhuuep.islandcraft.common.api;
+
+public interface ICWorld2 {
+    long getSeed();
+
+    int biomeId(String name);
+}
