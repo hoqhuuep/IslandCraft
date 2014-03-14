60b7636f739c8eaf1f72d7f1f9ea525ef2f7601e
hoqhuuep <daniel@genesis.net.au>
2014-03-14 17:50:10 +1100

    Implemented seconds for clock (not that anyone will use it...)

--- IslandCraft-Clock/src/main/java/com/github/hoqhuuep/islandcraft/clock/ClockManager.java
+++ IslandCraft-Clock/src/main/java/com/github/hoqhuuep/islandcraft/clock/ClockManager.java
@@ -13,11 +13,14 @@ import com.github.hoqhuuep.islandcraft.core.Message;
  * @version 2014-03-07
  */
 public class ClockManager {
-	private static final long ONE_DAY = 24000;
-	private static final long HOURS_PER_DAY = 24;
-	private static final long MINUTES_PER_HOUR = 60;
-	private static final long ONE_HOUR = ONE_DAY / HOURS_PER_DAY;
-	private static final long OFFSET = 6 * ONE_HOUR;
+	private static final double ONE_DAY = 24000.0;
+	private static final double HOURS_PER_DAY = 24.0;
+	private static final double MINUTES_PER_HOUR = 60.0;
+	private static final double SECONDS_PER_MINUTE = 60.0;
+	private static final double MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+	private static final double ONE_HOUR = ONE_DAY / HOURS_PER_DAY;
+	private static final double ONE_MINUTE = ONE_DAY / MINUTES_PER_DAY;
+	private static final double OFFSET = 6.0 * ONE_HOUR;
 
 	/**
 	 * Sends an appropriate message to a player who asks for the time. If they
@@ -30,9 +33,9 @@ public class ClockManager {
 		final Environment environment = world.getEnvironment();
 		if (environment == Environment.NORMAL) {
 			final long time = world.getTime();
-			final int hour = (int) (((time + OFFSET) % ONE_DAY) / ONE_HOUR);
-			final int minute = (int) ((((time + OFFSET) % ONE_HOUR) * MINUTES_PER_HOUR) / ONE_HOUR);
-			final int second = 0; // TODO calculate seconds
+			final int hour = (int) Math.floor(((time + OFFSET) % ONE_DAY) / ONE_HOUR);
+			final int minute = (int) Math.floor((((time + OFFSET) % ONE_HOUR) * MINUTES_PER_HOUR) / ONE_HOUR);
+			final int second = (int) Math.floor((((time + OFFSET) % ONE_MINUTE) * SECONDS_PER_MINUTE) / ONE_MINUTE);
 			final Calendar date = Calendar.getInstance();
 			date.set(Calendar.HOUR_OF_DAY, hour);
 			date.set(Calendar.MINUTE, minute);
