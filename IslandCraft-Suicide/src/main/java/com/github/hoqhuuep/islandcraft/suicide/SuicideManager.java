097946173bf8eb5297deb79a9607cb1db13abcd6
hoqhuuep <daniel@genesis.net.au>
2014-03-16 14:19:56 +1100

    Removed all non-island related features (Clock, Compass, Suicide, LocalChat, PartyChat, PrivateMessage)

--- IslandCraft-Suicide/src/main/java/com/github/hoqhuuep/islandcraft/suicide/SuicideManager.java
+++
@@ -1,9 +0,0 @@
-package com.github.hoqhuuep.islandcraft.suicide;
-
-import org.bukkit.entity.Player;
-
-public class SuicideManager {
-	public void suicide(final Player player) {
-		player.setHealth(0.0);
-	}
-}
